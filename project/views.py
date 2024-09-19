a2ecde06d66b305c4517b260828d7553e155591a
LionelMv <mvangicode@gmail.com>
2024-09-19 14:49:47 +0300

    feat: Add Customer and Order views
    Added these views for the api
    - CustomerListCreateView: gets all customers and creates a customer
    - CustomerRetrieveUpdateDestroyView: gets a specific customer, updates
      and deletes a customer
    - OrderListCreateView: gets all orders and creates an order.
    - OrderRetrieveUpdateDestroyView: gets a specific order, updates and
      deletes an order.

--- project/views.py
+++ project/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
 
-# Create your views here.
+
+# Customer Views
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+# Order Views
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    # Overriding the create method to associate the order with a customer
+    def perform_create(self, serializer):
+        customer_id = self.request.data.get('customer_id')
+        customer = Customer.objects.get(id=customer_id)
+        serializer.save(customer=customer)
+
+
+class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
