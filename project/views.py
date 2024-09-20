9465cc62a6fcb29e60e9f3e8a0e4ae1afd53dbe4
LionelMv <mvangicode@gmail.com>
2024-09-20 11:10:08 +0300

    feat: Remove perform_create method
    Removed the perform_create method and let the OrderSerializer class
    handle the customer_id creation for the order.

--- project/views.py
+++ project/views.py
@@ -19,12 +19,6 @@ class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    # Overriding the create method to associate the order with a customer
-    def perform_create(self, serializer):
-        customer_id = self.request.data.get('customer_id')
-        customer = Customer.objects.get(id=customer_id)
-        serializer.save(customer=customer)
-
 
 class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
