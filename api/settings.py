f63a84a1da2ce3678406a36d77941ae0140657f0
LionelMv <mvangicode@gmail.com>
2024-09-25 12:08:39 +0300

    config: Add defaults for DATABASE settings
    Added defaults for database settings to be in line with GitHub Actions
    workflows

--- api/settings.py
+++ api/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
+        'NAME': config("DB_NAME", default="testdb"),
+        'USER': config("DB_USER", default="postgres"),
+        'PASSWORD': config("DB_PASSWORD", default="postgres"),
+        'HOST': config("DB_HOST", default="localhost"),
+        'PORT': config("DB_PORT", default="5432"),
     }
 }
 
